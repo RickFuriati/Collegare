ae6e87809d86bc47dab687736ae0916e427aa80f
RickFuriati <90704425+RickFuriati@users.noreply.github.com>
2021-11-27 13:33:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+    
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -19.9233425, lng: -43.99377035},
+    zoom: 15
+  });
+  var input = document.getElementById('searchInput');
+  map.controls.push(input);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+          window.alert("Autocomplete's returned place contains no geometry");
+          return;
+      }
+      if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+      } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+      }
+      marker.setIcon(({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+  
+      var address = '';
+      if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+      }
+  
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    
+      
+      for (var i = 0; i < place.address_components.length; i++) {
+          if(place.address_components[i].types[0] == 'postal_code'){
+              document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+          }
+          if(place.address_components[i].types[0] == 'country'){
+              document.getElementById('country').innerHTML = place.address_components[i].long_name;
+          }
+      }
+      document.getElementById('location').innerHTML = place.formatted_address;
+      document.getElementById('lat').innerHTML = place.geometry.location.lat();
+      document.getElementById('lon').innerHTML = place.geometry.location.lng();
+  });
+}
